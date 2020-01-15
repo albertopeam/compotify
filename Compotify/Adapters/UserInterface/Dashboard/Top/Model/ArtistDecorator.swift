fbf9872cc59a84428f69243c8839143c66a0b0ca
Alberto Penas Amor <alberto.penas@nomasystems.com>
2020-01-15 23:12:50 +0100

    added Kingfisher library to load images added TopView structure

---
+++ Compotify/Adapters/UserInterface/Dashboard/Top/Model/ArtistDecorator.swift
@@ -0,0 +1,25 @@
+//
+//  ArtistDecorator.swift
+//  Compotify
+//
+//  Created by Alberto Penas Amor on 15/01/2020.
+//  Copyright © 2020 com.github.albertopeam. All rights reserved.
+//
+
+import Foundation
+
+struct ArtistDecorator: Identifiable {
+    let id: String
+    let name: String
+    let genres: String
+    let image: URL?
+    let popularity: String
+
+    init(_ artist: Artist) {
+        self.id = artist.id
+        self.name = artist.name
+        self.genres = artist.genres.joined(separator: ", ")
+        self.image = artist.image
+        self.popularity = String(artist.popularity)
+    }
+}
--- Compotify/Adapters/UserInterface/Dashboard/Top/TopView.swift
+++ Compotify/Adapters/UserInterface/Dashboard/Top/TopView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import Combine
+import KingfisherSwiftUI
 
 struct TopView: View {
 
@@ -15,7 +16,36 @@ struct TopView: View {
     @ObservedObject var viewModel: TopViewModel = .init()
     
     var body: some View {
-        Text("First View")
+        NavigationView {
+            //TODO: horizontal...
+            List {
+                ForEach(viewModel.items) { item in
+                    Text(item.name)
+                    KFImage(item.image) // image is not loading
+                        .resizable()
+                        .frame(width: 128, height: 128)
+                        .cornerRadius(20)
+                        .shadow(radius: 5)
+                }
+                /*ScrollView(.horizontal, content: {
+                    HStack(spacing: 10) {
+                        ForEach(viewModel.items) { item in
+                            Text(item.name)
+                            KFImage(item.image)
+                                .resizable()
+                                .frame(width: 128, height: 128)
+                                .cornerRadius(20)
+                                .shadow(radius: 5)
+                        }
+                    }
+                    .padding(.leading, 10)
+                })
+                .frame(height: 190)*/
+            }
+            .padding(.leading, -20)
+            .padding(.trailing, -20)
+        }.onAppear(perform: { self.viewModel.get() })
+        //TODO: think what todo next
         //artists & tracks???
         //start with artists?
         // publisher with the data...(not filtered...)
@@ -27,18 +57,24 @@ struct TopView: View {
     }
 }
 
+//TODO: move out of here
 class TopViewModel: ObservableObject {
-    let networkAdapter: NetworkAdapter = .init()
-    let authPort: AuthenticationPort = AuthenticationAdapter()
-    @Published var items: [Artist] = .init()
+    @Published var items: [ArtistDecorator] = .init()
+    private let networkAdapter: NetworkAdapter = .init()
+    private let authPort: AuthenticationPort = AuthenticationAdapter()
     private var subscriptions = Set<AnyCancellable>()
 
     init() {
+        //TODO: move injections to here
+    }
+
+    func get() {
         networkAdapter.myTopArtists(token: authPort.token)
-            .receive(on: RunLoop.main)
-            .sink(receiveCompletion: { print($0) }, //TODO: handle 401
-                  receiveValue: { self.items = $0; print($0) })
-            .store(in: &subscriptions)
+        .map({ $0.map({ ArtistDecorator($0) }) })
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { print($0) }, //TODO: handle 401
+              receiveValue: { self.items = $0 })
+        .store(in: &subscriptions)
     }
 }
 
